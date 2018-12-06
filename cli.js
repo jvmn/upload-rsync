cc449fed6b33c77bdfebffed21e7612ba81a8f09
Holger Prys <holger.prys@jvm.de>
2018-12-06 17:18:44 +0100

    Added promt with skip option --yes

--- cli.js
+++ cli.js
@@ -2,12 +2,34 @@
 'use strict';
 
 const transfer = require('./index');
+const inquirer = require('inquirer');
 
 try{
-    new transfer.rsync()
+    const rsync = new transfer.rsync()
         .init()
-        .setup()
-        .run();
+        .setup();
+
+    const config = rsync.config;
+
+    console.log("rsync command: \n" + rsync.rsync.command());
+
+    if (config.executeWithoutPrompt()) {
+        rsync.run();
+    } else {
+        inquirer.prompt([
+            {
+              type: 'confirm',
+              name: 'execute',
+              message: 'Start upload?',
+              default: true
+            }
+          ])
+          .then(answers => {
+            if (answers.execute) {
+                rsync.run();
+            }
+          });
+    }
 } catch (err) {
     console.log(err.stack)
 }
